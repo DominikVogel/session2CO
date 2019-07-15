923a6b7f2f50d432742d7e423d07b7f8e5673b7c
Dominik <post@vogel-online.info>
2019-07-15 16:14:08 +0200

    add dfs to avoid check warnings

--- R/session2CO.R
+++ R/session2CO.R
@@ -16,38 +16,39 @@ session2CO <- function(sess = NULL, path = paste0(getwd(), "/session2CO.txt")) {
 
   # Remove System and R version
   sess_df <- dplyr::filter(sess_df,
-                    grepl("^(.*)+(Windows >\\=)(.*)+", Description)==FALSE)
+                    grepl("^(.*)+(Windows >\\=)(.*)+", sess_df$Description)==FALSE)
   sess_df <- dplyr::filter(sess_df,
-                    grepl("^(.*)+(macOS )(.*)+", Description)==FALSE)
+                    grepl("^(.*)+(macOS )(.*)+", sess_df$Description)==FALSE)
   sess_df <- dplyr::filter(sess_df,
-                    grepl("^(R)$", Description)==FALSE)
+                    grepl("^(R)$", sess_df$Description)==FALSE)
 
   # Only packages from CRAN
-  sess_df_cran <- dplyr::filter(sess_df, Origin == "CRAN")
+  sess_df_cran <- dplyr::filter(sess_df, sess_df$Origin == "CRAN")
 
   # Create Rscript code for CRAN packages
   sess_df_cran <-
     dplyr::mutate(sess_df_cran,
                   code = paste0("RUN Rscript -e \'options(warn=2); devtools::install_version(\"",
-                                Description,
+                                sess_df_cran$Description,
                                 "\", version = \"",
-                                Version,
+                                sess_df_cran$Version,
                                 "\")\'"))
 
   # Only packages from GitHub
   sess_df_github <-
-    dplyr::filter(sess_df, grepl("^(Github)(.*)+$", Origin))
+    dplyr::filter(sess_df, grepl("^(Github)(.*)+$", sess_df$Origin))
 
   sess_df_github <-
     dplyr::mutate(sess_df_github,
-                  package = stringr::str_extract(Origin, "(?<=Github\\: )(.*)(?=@)"))
+                  package = stringr::str_extract(sess_df_github$Origin,
+                                                 "(?<=Github\\: )(.*)(?=@)"))
 
   sess_df_github <-
     dplyr::mutate(sess_df_github,
                   code = paste0("RUN Rscript -e \'devtools::install_gitub(\"",
-                                package,
+                                sess_df_github$package,
                                 "\", upgrade_dependencies = FALSE, ref = \"v",
-                                Version,
+                                sess_df_github$Version,
                                 "\")\'"))
 
   # Combine CRAN and GitHub
